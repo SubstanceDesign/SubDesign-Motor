423a0b87841471ee1c3d163d2be50adc498c02d9
Dzmitry Vasilevsky <dzm.vasilevsky@gmail.com>
2015-12-11 16:02:24 +0700

    Layout - all features

--- _source/core/layout/js/layout.js
+++ _source/core/layout/js/layout.js
@@ -5,7 +5,7 @@
 (function() {
 
   /* Base framework object with functions and usefull properties
-   ***********************************************/
+  ***********************************************/
   window.framework = {
     // caching often usable nodes
     'window': window,
@@ -13,14 +13,14 @@
     'html': document.documentElement,
 
     // add Class to node
-    'addClass': function(elem, className) {
+    'addClass': function( elem, className ) {
       if (elem.className.search(className) == '-1') {
         elem.className = elem.className ? elem.className + ' ' + className : className;
       }
     },
 
     // remove Class from node
-    'removeClass': function(elem, className) {
+    'removeClass': function( elem, className ) {
       var classPattern = '(^|\\s)' + className,
         re = new RegExp(classPattern, 'g');
       elem.className = elem.className.replace(re, '');
